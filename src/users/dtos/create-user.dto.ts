7751342bf2f17f75c93ee89835b4319cd2af0285
Amandeep Singh <amandeep90s@users.noreply.github.com>
2026-01-10 23:40:06 +0530

    feat(users): Creating create user DTO

---
+++ src/users/dtos/create-user.dto.ts
@@ -0,0 +1,40 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3, { message: 'First name must be at least 3 characters long' })
+  @MaxLength(50, { message: 'First name must be at most 50 characters long' })
+  firstName: string;
+
+  @IsString()
+  @IsOptional()
+  @MinLength(3, { message: 'Last name must be at least 3 characters long' })
+  @MaxLength(50, { message: 'Last name must be at most 50 characters long' })
+  lastName?: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8, { message: 'Password must be at least 8 characters long' })
+  @MaxLength(100, { message: 'Password must be at most 100 characters long' })
+  @Matches(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+    {
+      message:
+        'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character',
+    },
+  )
+  password: string;
+}
