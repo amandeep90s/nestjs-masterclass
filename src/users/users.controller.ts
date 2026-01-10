18c4d6225349416ea64a5a32955bad658448791e
Amandeep Singh <amandeep90s@users.noreply.github.com>
2026-01-10 16:28:07 +0530

    feat(users): Implement CRUD methods in UsersController

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get()
+  findAll() {
+    return 'This action returns all users';
+  }
+
+  @Get(':id')
+  findOne() {
+    return 'This action returns a user by id';
+  }
+
+  @Post()
+  create() {
+    return 'This action creates a new user';
+  }
+
+  @Put(':id')
+  update() {
+    return 'This action updates a user';
+  }
+
+  @Delete(':id')
+  remove() {
+    return 'This action removes a user';
+  }
+}
