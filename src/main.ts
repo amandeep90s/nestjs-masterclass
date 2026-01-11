2abeb66e06e6cb9d83abd005c8cbb95b15593814
Amandeep Singh <amandeep90s@users.noreply.github.com>
2026-01-11 21:56:26 +0530

    feat(swagger): Add Swagger module for API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -13,6 +14,12 @@ async function bootstrap() {
       forbidNonWhitelisted: true, // Throw an error if non-whitelisted properties are present
     }),
   );
+
+  // Swagger configuration
+  const config = new DocumentBuilder().setVersion('1.0').build();
+  // Instantiate swagger document
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
