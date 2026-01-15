dea10a5665bb02eac838537796a0d7efb6214ce2
Amandeep Singh <amandeep90s@users.noreply.github.com>
2026-01-15 18:00:19 +0530

    feat: Add global configuration and TypeORM setup for PostgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,32 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
+import { PostsModule } from './posts/posts.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule, PostsModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    UsersModule,
+    PostsModule,
+    AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      entities: [],
+      synchronize: process.env.DB_SYNCHRONIZE === 'true',
+      port: Number(process.env.DB_PORT),
+      host: process.env.DB_HOST,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
