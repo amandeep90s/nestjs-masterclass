e6fc16a2f405613cfd62c8f476eb342da4183fb2
Amandeep Singh <amandeep90s@users.noreply.github.com>
2026-01-15 12:54:24 +0530

    feat(posts): Update CreatePostDto to enforce stricter validation rules and modify metaOptions structure

--- src/posts/dtos/create-post.dto.ts
+++ src/posts/dtos/create-post.dto.ts
@@ -1,14 +1,62 @@
+import {
+  IsArray,
+  IsEnum,
+  IsISO8601,
+  IsJSON,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUrl,
+  Matches,
+  MinLength,
+} from 'class-validator';
 import { EPostStatus, EPostType } from '../enums';
 
 export class CreatePostDto {
+  @IsString()
+  @MinLength(4, { message: 'Title must be at least 4 characters long' })
+  @IsNotEmpty({ message: 'Title is required' })
   title: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'Slug is required' })
+  @Matches(/^[a-z0-9]+(?:-[a-z0-9]+)*$/, {
+    message: 'A slug should be all small letters and hyphens only',
+  })
   slug: string;
+
+  @IsEnum(EPostType, { message: 'Invalid post type' })
+  @IsNotEmpty({ message: 'Post type is required' })
   postType: EPostType;
+
+  @IsString()
+  @IsOptional()
   content?: string;
+
+  @IsEnum(EPostStatus, { message: 'Invalid post status' })
+  @IsNotEmpty({ message: 'Post status is required' })
   status: EPostStatus;
+
+  @IsOptional()
+  @IsJSON()
   schema?: string;
+
+  @IsOptional()
+  @IsUrl()
   featuredImageUrl?: string;
+
+  @IsISO8601()
+  @IsOptional()
   publishedOn?: Date;
+
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true, message: 'Each tag must be a string' })
+  @MinLength(3, {
+    each: true,
+    message: 'Each tag must be at least 3 characters long',
+  })
   tags?: string[];
-  metaOptions?: [{ key: string; value: string }];
+
+  metaOptions?: [{ key: 'sidebarEnabled'; value: true }];
 }
