06d1c903233b5a0b720e8b8cee5488133df27c1b
Amandeep Singh <amandeep90s@users.noreply.github.com>
2026-02-01 21:59:09 +0530

    feat(users): Register AccessTokenGuard as a global guard in UsersModule

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,11 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
 import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from 'src/auth/auth.module';
 import jwtConfig from 'src/auth/config/jwt.config';
+import { AccessTokenGuard } from 'src/auth/guards/access-token/access-token.guard';
 import profileConfig from './config/profile.config';
 import { CreateUserProvider } from './providers/create-user.provider';
 import { FindOneUserByEmailProvider } from './providers/find-one-user-by-email.provider';
@@ -19,6 +21,10 @@ import { UsersController } from './users.controller';
     UsersCreateManyProvider,
     CreateUserProvider,
     FindOneUserByEmailProvider,
+    {
+      provide: APP_GUARD,
+      useClass: AccessTokenGuard,
+    },
   ],
   exports: [UsersService, FindOneUserByEmailProvider],
   imports: [
