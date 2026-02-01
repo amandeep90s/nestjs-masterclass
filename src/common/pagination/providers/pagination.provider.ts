2989aad55322d3959ceccaeced5b309548871694
Amandeep Singh <amandeep90s@users.noreply.github.com>
2026-02-01 16:10:21 +0530

    feat(pagination): Enhance PaginationProvider to inject request for URL creation and improve pagination logic

--- src/common/pagination/providers/pagination.provider.ts
+++ src/common/pagination/providers/pagination.provider.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { REQUEST } from '@nestjs/core';
+import { type Request } from 'express';
 import { ObjectLiteral, Repository } from 'typeorm';
 import { PaginationQueryDto } from '../dtos/pagination-query.dto';
 
 @Injectable()
 export class PaginationProvider {
+  constructor(
+    /**
+     * Injecting request to create request urls
+     */
+    @Inject(REQUEST)
+    private readonly request: Request,
+  ) {}
+
   public async paginateQuery<T extends ObjectLiteral>(
     paginationQuery: PaginationQueryDto,
     repository: Repository<T>,
@@ -14,6 +24,26 @@ export class PaginationProvider {
         ? (paginationQuery.page - 1) * paginationQuery.limit
         : 0,
     });
+
+    /**
+     * Create the request urls
+     */
+    const baseUrl = `${this.request.protocol}://${this.request.headers.host}/`;
+    const newUrl = new URL(this.request.url, baseUrl);
+
+    /**
+     * Calculating page numbers
+     */
+    const totalItems = await repository.count();
+    const totalPages = Math.ceil(totalItems / paginationQuery.limit);
+    const nextPage =
+      paginationQuery.page === totalPages
+        ? paginationQuery.page
+        : paginationQuery.page + 1;
+    const prevPage =
+      paginationQuery.page === 1
+        ? paginationQuery.page
+        : paginationQuery.page - 1;
 
     return results;
   }
