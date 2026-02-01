5261892cb956ebbbfcf84b66cd858ebdb7b38ae8
Amandeep Singh <amandeep90s@users.noreply.github.com>
2026-02-01 18:48:53 +0530

    feat(auth): Add HTTP status code response for sign-in endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { SignInDto } from './dtos/signin.dto';
 import { AuthService } from './providers/auth.service';
@@ -9,6 +9,7 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('sign-in')
+  @HttpCode(HttpStatus.OK)
   public async signIn(@Body() signInDto: SignInDto) {
     return await this.authService.signIn(signInDto);
   }
