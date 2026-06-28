3e987ef9cc091f1881ef8b355a4327bff67a270f
Amandeep Singh <amandeep90s@users.noreply.github.com>
2026-06-28 22:38:14 +0530

    feat: implement generateTokens method for JWT token generation

--- src/auth/providers/generate-tokens.provider.ts
+++ src/auth/providers/generate-tokens.provider.ts
@@ -1,7 +1,9 @@
 import { Inject, Injectable } from '@nestjs/common';
 import type { ConfigType } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
+import { User } from 'src/users/user.entity';
 import jwtConfig from '../config/jwt.config';
+import { IActiveUserData } from '../interfaces/active-user-data.interface';
 
 @Injectable()
 export class GenerateTokensProvider {
@@ -33,5 +35,26 @@ export class GenerateTokensProvider {
     );
 
     return token;
+  }
+
+  public async generateTokens(user: User) {
+    const [accessToken, refreshToken] = await Promise.all([
+      // Generate access token
+      this.signToken<Partial<IActiveUserData>>(
+        user.id,
+        this.jwtConfiguration.accessTokenTtl,
+        {
+          email: user.email,
+        },
+      ),
+
+      // Generate refresh token
+      this.signToken(user.id, this.jwtConfiguration.refreshTokenTtl),
+    ]);
+
+    return {
+      accessToken,
+      refreshToken,
+    };
   }
 }
