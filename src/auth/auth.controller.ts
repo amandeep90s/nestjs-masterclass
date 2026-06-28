2cb2d74d8d646b81045b71d53052738076812feb
Amandeep Singh <amandeep90s@users.noreply.github.com>
2026-06-28 23:07:09 +0530

    feat: implement refresh token endpoint and service integration

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { Auth } from './decorators/auth.decorator';
+import { RefreshTokenDto } from './dtos/refresh-token.dto';
 import { SignInDto } from './dtos/signin.dto';
 import { EAuthType } from './enums/auth-type.enum';
 import { AuthService } from './providers/auth.service';
@@ -15,5 +16,12 @@ export class AuthController {
   @Auth(EAuthType.None)
   public async signIn(@Body() signInDto: SignInDto) {
     return await this.authService.signIn(signInDto);
+  }
+
+  @Post('refresh-token')
+  @HttpCode(HttpStatus.OK)
+  @Auth(EAuthType.None)
+  public async refreshToken(@Body() refreshTokenDto: RefreshTokenDto) {
+    return await this.authService.refreshToken(refreshTokenDto);
   }
 }
--- src/auth/providers/auth.service.ts
+++ src/auth/providers/auth.service.ts
@@ -1,6 +1,8 @@
 import { forwardRef, Inject, Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/providers/users.service';
+import { RefreshTokenDto } from '../dtos/refresh-token.dto';
 import { SignInDto } from '../dtos/signin.dto';
+import { RefreshTokensProvider } from './refresh-tokens.provider';
 import { SignInProvider } from './sign-in.provider';
 
 @Injectable()
@@ -16,6 +18,11 @@ export class AuthService {
      * Injecting SignInProvider to handle sign-in logic
      */
     private readonly signInProvider: SignInProvider,
+
+    /**
+     * Injecting RefreshTokensProvider to handle refresh token logic
+     */
+    private readonly refreshTokensProvider: RefreshTokensProvider,
   ) {}
 
   public async signIn(signInDto: SignInDto) {
@@ -24,5 +31,9 @@ export class AuthService {
 
   public isAuthenticated(token: string): boolean {
     return token === '123';
+  }
+
+  public async refreshToken(refreshTokenDto: RefreshTokenDto) {
+    return await this.refreshTokensProvider.refreshTokens(refreshTokenDto);
   }
 }
