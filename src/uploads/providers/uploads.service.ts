49ad31fbd864e2ae3f92cba4dae21b6e9039ce91
Amandeep Singh <amandeep90s@users.noreply.github.com>
2026-07-07 13:15:17 +0530

    Add upload file endpoint and service method

--- src/uploads/providers/uploads.service.ts
+++ src/uploads/providers/uploads.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UploadsService {}
+export class UploadsService {
+  public async uploadFile(file: Express.Multer.File): Promise<string> {
+    // Upload to the AWS S3 bucket
+    // Generate a new entry in the database
+    // Return the URL of the uploaded file
+  }
+}
