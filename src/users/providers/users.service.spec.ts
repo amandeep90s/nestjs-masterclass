4606df6ebba81e27381ff1b759b07ddafa7b6346
Amandeep Singh <amandeep90s@users.noreply.github.com>
2026-07-08 15:17:10 +0530

    Refactor UsersService dependencies and test setup
    Remove unused AuthService and config injections from UsersService, and
    update the unit test module to provide all required provider
    dependencies as mocks. Also add Prettier printWidth set to 100.

--- src/users/providers/users.service.spec.ts
+++ src/users/providers/users.service.spec.ts
@@ -1,12 +1,39 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { CreateGoogleUserProvider } from './create-google-user.provider';
+import { FindOneByGoogleIdProvider } from './find-one-by-google-id.provider';
+import { FindOneUserByEmailProvider } from './find-one-user-by-email.provider';
+import { CreateUserProvider } from './create-user.provider';
+import { UsersCreateManyProvider } from './users-create-many.provider';
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: CreateGoogleUserProvider,
+          useValue: {},
+        },
+        {
+          provide: FindOneByGoogleIdProvider,
+          useValue: {},
+        },
+        {
+          provide: FindOneUserByEmailProvider,
+          useValue: {},
+        },
+        {
+          provide: CreateUserProvider,
+          useValue: {},
+        },
+        {
+          provide: UsersCreateManyProvider,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
