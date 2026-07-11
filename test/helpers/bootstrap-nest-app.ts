3734b4b838cc8e6a923d80804f5c61ae9f853694
Amandeep Singh <65186944+amandeep90s@users.noreply.github.com>
2026-07-11 20:48:09 +0530

    refactor: extract app bootstrap logic into a separate helper function

---
+++ test/helpers/bootstrap-nest-app.ts
@@ -0,0 +1,21 @@
+import { INestApplication } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Test, TestingModule } from '@nestjs/testing';
+import { appCreate } from 'src/app.create';
+import { AppModule } from 'src/app.module';
+import { App } from 'supertest/types';
+
+export async function bootstrapNestApp(): Promise<INestApplication<App>> {
+  const moduleFixture: TestingModule = await Test.createTestingModule({
+    imports: [AppModule, ConfigModule],
+    providers: [ConfigService],
+  }).compile();
+
+  const app: INestApplication<App> = moduleFixture.createNestApplication();
+
+  appCreate(app);
+
+  await app.init();
+
+  return app;
+}
--- test/users/users.post.e2e-spec.ts
+++ test/users/users.post.e2e-spec.ts
@@ -1,9 +1,7 @@
 import { INestApplication } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { Test, TestingModule } from '@nestjs/testing';
-import { appCreate } from 'src/app.create';
-import { AppModule } from 'src/app.module';
+import { ConfigService } from '@nestjs/config';
 import { App } from 'supertest/types';
+import { bootstrapNestApp } from '../helpers/bootstrap-nest-app';
 import { dropDatabase } from '../helpers/drop-database';
 
 jest.mock('@nestjs-modules/mailer/adapters/handlebars.adapter', () => ({
@@ -15,15 +13,8 @@ describe('[Users] @Post Endpoints', () => {
   let config: ConfigService;
 
   beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule, ConfigModule],
-      providers: [ConfigService],
-    }).compile();
-
-    app = moduleFixture.createNestApplication();
-    appCreate(app);
+    app = await bootstrapNestApp();
     config = app.get<ConfigService>(ConfigService);
-    await app.init();
   });
 
   afterEach(async () => {
